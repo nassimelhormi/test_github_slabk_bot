7700e1d6fa7ea411a53f2158932e46be977c2acb
Nassim <nassim.elhormi@dailymotion.com>
2022-10-19 16:50:02 +0200

    feat(github): first version

--- main/main.py
+++ main/main.py
@@ -1,10 +1,23 @@
 from utils.github import GithubClient
+from utils.slack import SlackClient
+from repos.list import repos
 import asyncio
 
 
 def main():
     github_client = GithubClient()
-    asyncio.run(github_client.get_pr_staging("nassimelhormi", "test_github_slabk_bot"))
+    slack_client = SlackClient()
+
+    message = ""
+    prs = []
+    for repo in repos:
+        prs.append(asyncio.run(github_client.get_pr_staging("nassimelhormi", repo)))
+
+    for pr in prs:
+        for info in pr:
+            message += info["html_url"] + "\n"
+
+    slack_client.send_slack_message(message="Hello <!here> we have those PRs in staging this week : \n" + message)
 
 
 if __name__ == '__main__':
---
+++ main/repos/list.py
@@ -0,0 +1,7 @@
+"""
+List of GitHub repositories that we want to track PR's.
+"""
+repos = [
+    "test_github_slabk_bot",
+    "test_slack_bot_github_2",
+]
